d2a8b06b96ed85be914e8cf4adba65513e5f7dfa
Aleksandr Minakov <am@infodrive.pro>
2023-08-02 17:33:07 +0300

    Task01 is worked correctly

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Please input first number");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please input second number");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+int square = secondnumber*secondnumber;
+
+if (firstnumber==square)
+{
+    Console.WriteLine("Yes! The first number is square of the second number");  
+}
+else
+{
+    Console.WriteLine("No! The first number is not a square of the second number");
+}
