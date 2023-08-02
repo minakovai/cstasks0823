0f7540463b5490bd1201e4049b0575c10abe96c5
Aleksandr Minakov <am@infodrive.pro>
2023-08-02 17:53:56 +0300

    The program is worked correctly

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Пожалуйста, введите день недели");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day>=1 && day<=7)
+{
+
+if (day==1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (day==2)
+{
+    Console.WriteLine("Вторник");
+}
+if (day==3)
+{
+    Console.WriteLine("Среда");
+}
+if (day==4)
+{
+    Console.WriteLine("Четверг");
+}
+if (day==5)
+{
+    Console.WriteLine("Пятница");
+}
+if (day==6)
+{
+    Console.WriteLine("Суббота");
+}
+if (day==7)
+{
+    Console.WriteLine("Воскресенье");
+}
+}
+else 
+{
+    Console.WriteLine("Некорректный ввод");
+}
