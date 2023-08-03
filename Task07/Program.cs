eef4d541e6d75efcf118f6bfdbd8687e926a4980
Aleksandr Minakov <am@infodrive.pro>
2023-08-03 08:51:00 +0300

    The program work correctly

---
+++ Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastdigit = number%10;
+Console.WriteLine(lastdigit);
