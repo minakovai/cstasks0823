8743cba4129f9886b6e9c5f3bec5481cdbda8cb3
Aleksandr Minakov <am@infodrive.pro>
2023-08-03 08:41:06 +0300

    The program work correctly

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 5. Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+Console.WriteLine("Пожалуйста, введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int mn = n * (-1);
+while (mn<=n)
+{
+    Console.Write($"{mn}, " ); 
+    mn++;
+}
