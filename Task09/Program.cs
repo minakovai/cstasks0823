535f44a7faf3420a972c0363859c2f590d1865f5
Alexandr Minakov <am@infodrive.pro>
2023-08-04 20:47:07 +0300

    Доработка проекта через методы

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -6,13 +6,24 @@
 // 85 -> 8
 
 
-int number = new Random().Next(10,100);
+int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число в диапозоне 10 -99 -> {number}");
-int firstDigit = number / 10; // 78/10=7
-int secondDigit = number % 10; // 78%10 = 70+8
+// int firstDigit = number / 10; // 78/10=7
+// int secondDigit = number % 10; // 78%10 = 70+8
 
 //if (firstDigit > secondDigit) Console.WriteLine ($"Наибольшая цифра числа -> {firstDigit}");
 //else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
 
-int result = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.WriteLine($"Наибольшая цифра числа -> {result}");
+// int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа -> {result}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10; // 78/10=7
+    int secondDigit = num % 10; // 78%10 = 70+8
+    //int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;;
+}
