5dad6afb62d894f4178dd9170f55fdb49fb35d70
Alexandr Minakov <am@infodrive.pro>
2023-08-28 22:15:18 +0300

    Add good working task 18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// 18. На пишите программу, которая:
+// 1. принимает номер четверти,
+// 2. по заданному номеру четверти выдает диапозон x и y
+
+string CoordinateRange(int num) 
+{
+    if (num == 1) return "x > 0 , y > 0";
+    if (num == 2) return "x < 0 , y > 0";
+    if (num == 3) return "x < 0 , y > 0"; 
+    if (num == 4) return "x > 0 , y < 0";
+    return null;
+}
+
+Console.WriteLine("Введите номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = CoordinateRange(number);
+Console.Write(result != null ? result : "Некорректный ввод");
+
