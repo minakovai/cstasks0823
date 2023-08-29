4b65ad50e98f97731f5ccc22c6d35d5e34f97970
Alexandr Minakov <am@infodrive.pro>
2023-08-29 11:24:38 +0300

    Add Task 19 work good

---
+++ Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿
+bool CheckFiveDigits (int num1)
+{
+    if ((num1 > 9999) & (num1 < 100000)) return true;
+    else return false;
+}
+
+void CheckPolindrom(int num)
+{
+    if (num / 10000 == num % 10)
+    {
+        if (((num / 1000) % 10) == ((num / 10) % 10))
+            Console.WriteLine($"{num} -> да");
+        else Console.WriteLine($"{num} -> нет");
+    }
+    else Console.WriteLine($"{num} -> нет");
+}
+
+
+Console.Write("Введите пятизначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (CheckFiveDigits(number) == false) Console.WriteLine("Вы ввели не пятизначное число");
+else CheckPolindrom(number);
