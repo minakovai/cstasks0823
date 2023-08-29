d8c82e49f8831ba8ea7ca654ffcf15145347bc79
Alexandr Minakov <am@infodrive.pro>
2023-08-29 12:05:56 +0300

    Add good working Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23. Напишите программу, которая:
+// 1. Принимает на вход число (N) 
+// 2. Выдает таблицу кубов чисел от 1 до  N.
+
+
+void TableCube(int number)
+{
+    int i = 1;
+    while (i <= number)
+    {
+        Console.WriteLine($"{i,3} -> {i * i * i,6}");
+        i++;
+    }
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+TableCube(n);
