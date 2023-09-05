ec541371ecff260de9425397f2fa232ea5ec57d9
Alexandr Minakov <am@infodrive.pro>
2023-09-05 21:53:50 +0300

    Add  Task 49  good working

---
+++ Task48/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+
+
+int[,] CreateMatrixSumIndexes(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    
+    for (int i=0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d = CreateMatrixSumIndexes(5,5);
+PrintMatrix(array2d);
